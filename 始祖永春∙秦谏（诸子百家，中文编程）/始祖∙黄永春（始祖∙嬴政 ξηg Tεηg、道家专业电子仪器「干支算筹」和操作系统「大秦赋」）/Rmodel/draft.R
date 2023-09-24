6c0bcc08c01c086274f6401969e245684d11d3ba
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2023-09-24 11:09:53 +0800

    《🚩春秋战国之鬼谷传奇》
    《🚩春秋战国之鬼谷传奇》
    
    《🚩🇨🇳🔆🏹🌟👊🚀🦔大秦赋 - 黄埔军校赢家黄氏永春堂 - 中科红旗，量化对冲》
    
    🚩🇨🇳🏹🦔量化对冲，中科红旗；一带一路，一统天下（兵马未动，粮草先行；人民币货币经济学，东亚中国司马错入蜀；攻占东南亚，提示大象共和国US白宫回教堂法官国父Judge Washington法家治国论，诸MY大汉山土司兼赤帝白骨精番邦·殡·刘九族和东南亚所有违反可兰经习俗文化宗教语言断肢法的Bahasa非法巫贼巫婆宦官东施民族回教徒）
    
    🚩🇨🇳🏹🦔诸子百家：儒学、墨学、法学、道学、兵法、阴阳学、佛学、哲学、运筹学、思想录、干支算盘、大秦赋
    
    🚩🇨🇳🏹🦔《大秦赋 - 🇨🇳关雎》
    🚩🇨🇳🏹🦔春秋战国，诸子百家；
    🚩🇨🇳🏹🦔左氏春秋，鬼谷传奇。
    🚩🇨🇳🏹🦔商鞅变法，道法兵家；
    🚩🇨🇳🏹🦔四面楚歌，焚经坑番。
    🚩🇨🇳🏹🦔一带一路，横跨七洲；
    🚩🇨🇳🏹🦔史无前例，一统天下。
    🚩🇨🇳🏹🦔高频量化，对冲基金；
    🚩🇨🇳🏹🦔只争朝夕，不负韶华。
    🚩🇨🇳🏹🦔学海无涯，唯勤是岸；
    🚩🇨🇳🏹🦔莫忘初衷，方得始终。
    https://gitee.com/eglianhu

---
+++ 始祖永春∙秦谏（诸子百家，中文编程）/始祖∙黄永春（始祖∙嬴政 ξηg Tεηg、道家专业电子仪器「干支算筹」和操作系统「大秦赋」）/Rmodel/draft.R
@@ -0,0 +1,41 @@
+## How to create a R package
+## https://www.youtube.com/watch?v=MoszELQFrvQ
+
+install.packages(c("aqp", "bayestestR", "bbmle", "cNORM", "credentials", "e1071", 
+                   "echarts4r", "fastmatch", "ggmulti", "gss", "hardhat", 
+                   "HMMextra0s", "jpeg", "matrixcalc", "maxLik", "mlr3misc", 
+                   "modeldata", "modeltime", "paletteer", "parsnip", "performance", 
+                   "phangorn", "pkgKitten", "posterior", "pryr", "ranger", 
+                   "rbibutils", "rbokeh", "RcppAnnoy", "RcppArmadillo", 
+                   "RcppCWB", "RcppSpdlog", "renv", "rgl", "rsconnect", 
+                   "Rttf2pt1", "shinydashboardPlus", "shinyMobile", 
+                   "stringdist", "targets", "testthat", "tune", "tzdb", 
+                   "vroom", "waiter", "workflows", "workflowsets", "x13binary", 
+                   "countrycode", "echarts4r", "foghorn", "IRdisplay", "IRkernel", 
+                   "MNLpred", "paletteer", "pbdZMQ", "reprex", "rsconnect", 
+                   "showtext", "sysfonts", "tsBSS"), 
+                 lib='/usr/lib/R/library', dependencies = TRUE, 
+                 'https://cloud.r-project.org', 
+                 INSTALL_opts=c('--no-multiarch', '--no-lock'))
+
+install.packages(c("aqp", "bayestestR", "cNORM", "credentials", "e1071", 
+                   "echarts4r", "fastmatch", "ggmulti", "gss", "hardhat", 
+                   "HMMextra0s", "jpeg", "matrixcalc", "maxLik", "mlr3misc", 
+                   "modeldata", "modeltime", "paletteer", "parsnip", 
+                   "performance", "phangorn", "pkgKitten", "posterior", 
+                   "pryr", "ranger", "rbibutils", "rbokeh", "RcppAnnoy", 
+                   "RcppArmadillo", "RcppCWB", "RcppSpdlog", "renv", 
+                   "reprex", "rgl", "rsconnect", "Rttf2pt1", "shinydashboardPlus", 
+                   "shinyMobile", "stringdist", "targets", "testthat", "tune", 
+                   "tzdb", "vroom", "waiter", "workflows", "workflowsets", 
+                   "x13binary", "countrycode", "echarts4r", "foghorn", 
+                   "IRdisplay", "IRkernel", "MNLpred", "paletteer", "pbdZMQ", 
+                   "reprex", "rsconnect", "showtext", "sysfonts", "tsBSS"), 
+                 lib = '/usr/lib/R/library', 'https://cloud.r-project.org', 
+                 dependencies = TRUE, INSTALL_opts = '--no-lock')
+
+install.packages(
+  c('xfun', 'tzdb', 'voom', 'rmarkdown', 'Rcpp', 'reprex'), 
+  lib = '/usr/lib/R/library', 'https://cloud.r-project.org', 
+  dependencies = TRUE, INSTALL_opts = '--no-lock')
+
