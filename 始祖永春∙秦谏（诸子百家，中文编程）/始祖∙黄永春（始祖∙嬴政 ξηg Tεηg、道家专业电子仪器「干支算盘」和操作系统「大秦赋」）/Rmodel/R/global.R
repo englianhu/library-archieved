02293fd14a13758cbfec2ed9a0fb52599b8f00d7
®γσ, ξηg Lιαη Ημ（雷欧） <englianhu@gmail.com>
2023-09-24 23:18:19 +0800

    《🚩春秋战国之鬼谷传奇》
    《🚩春秋战国之鬼谷传奇》
    
    《🚩🇨🇳🔆🏹🌟👊🚀🦔大秦赋 - 黄埔军校赢家黄氏永春堂 - 中科红旗，量化对冲》
    
    🚩🇨🇳🏹🦔量化对冲，中科红旗；一带一路，一统天下（兵马未动，粮草先行；人民币货币经济学，东亚中国司马错入蜀；攻占东南亚，提示大象共和国US白宫回教堂法官国父Judge Washington法家治国论，诸MY大汉山土司兼赤帝白骨精番邦·殡·刘九族和东南亚所有违反可兰经习俗文化宗教语言断肢法的Bahasa非法巫贼巫婆宦官东施民族回教徒）
    
    🚩🇨🇳🏹🦔诸子百家：儒学、墨学、法学、道学、兵法、阴阳学、佛学、哲学、运筹学、思想录、干支算盘、大秦赋
    
    🚩🇨🇳🏹🦔《大秦赋 - 🇨🇳关雎》
    🚩🇨🇳🏹🦔春秋战国，诸子百家；
    🚩🇨🇳🏹🦔左氏春秋，鬼谷传奇。
    🚩🇨🇳🏹🦔商鞅变法，道法兵家；
    🚩🇨🇳🏹🦔四面楚歌，焚经坑番。
    🚩🇨🇳🏹🦔一带一路，横跨七洲；
    🚩🇨🇳🏹🦔史无前例，一统天下。
    🚩🇨🇳🏹🦔高频量化，对冲基金；
    🚩🇨🇳🏹🦔只争朝夕，不负韶华。
    🚩🇨🇳🏹🦔学海无涯，唯勤是岸；
    🚩🇨🇳🏹🦔莫忘初衷，方得始终。
    https://gitee.com/eglianhu

--- 始祖永春∙秦谏（诸子百家，中文编程）/始祖∙黄永春（始祖∙嬴政 ξηg Tεηg、道家专业电子仪器「干支算盘」和操作系统「大秦赋」）/Rmodel/R/global.R
+++
@@ -1,3 +0,0 @@
-utils::globalVariables(c("MatchID_7M", "MatchID_NG", "Round", "KODate", "Home", "Away", "FTHG", "FTAG", "HTHG", "HTAG"))
-methods::setOldClass(c("tbl_ts", "tbl_df", "tbl", "data.frame"))
-
--- 始祖永春∙秦谏（诸子百家，中文编程）/始祖∙黄永春（始祖∙嬴政 ξηg Tεηg、道家专业电子仪器「干支算盘」和操作系统「大秦赋」）/Rmodel/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(wk4package)
-
-test_check('Rmodel')
